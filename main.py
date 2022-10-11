b68d70019ecb57c30315bd7762574facc8c1ae2e
Bang <nkbang@gmail.com>
2022-10-11 09:08:41 +0700

    Fix: - Avoid parsing plain text email as HTML - Remove anchor tag with name attribute to prevent attachments to disappear

--- main.py
+++ main.py
@@ -133,10 +133,11 @@ def parse_attachments(parsed_eml, ep, body):
       if content_type.lower().startswith('message/rfc822'):
         nested_raw_email = base64.b64decode(attachment.get('raw'))
         nested_parsed_eml = ep.decode_email_bytes(nested_raw_email)
-        for nested_attachment in nested_parsed_eml.get('attachment'):
-          if (nested_attachment.get('content_header').get('content-id') 
-              and len(nested_attachment.get('content_header').get('content-id')) > 0):
-            nested_content_ids = nested_content_ids + nested_attachment.get('content_header').get('content-id')
+        if (nested_parsed_eml.get('attachment') != None):
+          for nested_attachment in nested_parsed_eml.get('attachment'):
+            if (nested_attachment.get('content_header').get('content-id') 
+                and len(nested_attachment.get('content_header').get('content-id')) > 0):
+              nested_content_ids = nested_content_ids + nested_attachment.get('content_header').get('content-id')
 
     for attachment in parsed_eml.get('attachment'):
       # attachment['raw'] is not actually raw. They are base64 encoded by eml_parser before returning to us.
@@ -171,8 +172,11 @@ def parse_attachments(parsed_eml, ep, body):
           attachment_filename = attachment.get('filename')
           content_type = attachment.get('content_header').get('content-type')[0]
           # when the attachment is an email but the filename does not contain an extension
-          if content_type.lower().startswith('message/rfc822') and '.' not in attachment_filename:
-            attachment_filename = 'Mail Attachment.eml'
+          if '.' not in attachment_filename:
+            if content_type.lower().startswith('message/rfc822'):
+              attachment_filename = 'Mail Attachment.eml'
+            elif content_type.lower().startswith('image/'):
+              attachment_filename = attachment.get('filename') + '.' + content_type.lower()[len('image/'):]
 
           file = NamedBytesIO(base64.b64decode(attachment.get('raw')), name=attachment_filename)
           attachments.append(Attachment(file_obj=file))
@@ -261,74 +265,89 @@ def convert(filename):
   
   # Parse body
   bodies = {}
-  any_content = None
-  for body in parsed_eml['body']:
+  # we only parse for the first two bodies for content type text/plain and text/html
+  # because some emails are plain and contain an other emails in the attachments
+  for body in parsed_eml['body'][:2]:
     if (body.get('content_type') != None):
       if (bodies.get(body.get('content_type')) == None): # only accept the first of each content_type if there are multiple
         bodies[body.get('content_type')] = body.get('content')
-    else:
-      any_content = body.get('content')
+    
   content_string = bodies.get(TEXT_PLAIN) if bodies.get(TEXT_HTML) == None else bodies.get(TEXT_HTML)
-  if (content_string == None): 
-    content_string = any_content
+  if (content_string == None): # when there is no body content as text/plain or text/html, we use no type content
+    no_type_content = ''
+    for body in parsed_eml['body']:
+      if (body.get('content_type') == None):
+        no_type_content = no_type_content + body.get('content')
+      else:
+        break
+    content_string = no_type_content
 
   attachments, attachment_filenames, content_string = parse_attachments(parsed_eml, ep, content_string)
   header = generate_header(parsed_eml, attachment_filenames)
 
-  elements = html5lib.parse(content_string, namespaceHTMLElements=False)
-  if len(attachments) > 0: 
-    # the ID attribute in the img tag cause attachments failed to be attached, so we need to delete it
-    # See https://github.com/Kozea/WeasyPrint/issues/1733 
-    # parse HTML to find any element with ID attributes and delete the id attribute
-    elements_with_ids = elements.findall('*//*[@id]')
-    for element in elements_with_ids:
-      del element.attrib['id']
-
-  # element with display:inline-block and width:100% can create truncate issue in rendering PDF
-  # here if any element has both of those styles, we remove display:inline-block
-  elements_with_styles = elements.findall('*//*[@style]')
-  for element in elements_with_styles:
-    declarations = tinycss2.parse_declaration_list(element.attrib['style'])
-    found_width_100_percent = False
-    found_display_inline_block = False
-    for d in declarations:
-      if check_attribute(d, 'name', 'display') and hasattr(d, 'value') and len(d.value) > 0 and check_attribute(d.value[0], 'lower_value', 'inline-block'):
-        found_display_inline_block = True
-      if check_attribute(d, 'name', 'width') and hasattr(d, 'value') and len(d.value) > 0 and check_attribute(d.value[0], 'type', 'percentage') and check_attribute(d.value[0], 'value', 100):
-        found_width_100_percent = True
-      
-    if found_width_100_percent and found_display_inline_block:
-      new_styles = []
-      for d in declarations:
-        if not (hasattr(d, 'name') and d.name == 'display' and len(d.value) > 0 and d.value[0].lower_value == 'inline-block'):
-          new_styles.append(d.serialize())
-      element.attrib['style'] = ';'.join(new_styles)
-
-  # insert header to first child body element if possible
-  # we don't insert to body element because in some emails the first element is a page. 
-  # so anything we put before the first element will push the first element to the 2nd page. 
-  header_inserted = False
-  body_element = elements.find('body') or elements.find('*//body')
-  if (body_element != None):
-    header_elements = html5lib.parse(header, namespaceHTMLElements=False)
-    for child in body_element:
-      child.insert(0, header_elements)
-      # if there is text in child, we want the header element to stay before the text
-      header_elements.tail, child.text = child.text, None
-      header_inserted = True
-      break
-
-  # produce the new HTML string after removing IDs attributes
-  s = html5lib.serializer.HTMLSerializer()
-  walker = html5lib.getTreeWalker("etree")
-  stream = walker(elements)
-  output = s.serialize(stream)
-  if not header_inserted:
-    content_string = header
+  if bodies.get(TEXT_HTML) == None:
+    content_string = "<pre>%s</pre>" % html.escape(content_string)
+    content_string = header + content_string
   else:
-    content_string = ''
-  for item in output:
-    content_string = content_string + item
+    elements = html5lib.parse(content_string, namespaceHTMLElements=False)
+    if len(attachments) > 0: 
+      # Because of bug: https://github.com/Kozea/WeasyPrint/issues/1733 
+      # cause attachments failed to be attached, so we need to look for:
+      # - Any tags with ID attribute
+      # - Anchor tag with name attribute
+      # parse HTML to find the above cases and delete the attributes
+      elements_with_ids = elements.findall('*//*[@id]')
+      for element in elements_with_ids:
+        del element.attrib['id']
+      anchor_with_names = elements.findall('*//a[@name]')
+      for element in anchor_with_names:
+        del element.attrib['name']
+
+    # element with display:inline-block and width:100% can create truncate issue in rendering PDF
+    # here if any element has both of those styles, we remove display:inline-block
+    elements_with_styles = elements.findall('*//*[@style]')
+    for element in elements_with_styles:
+      declarations = tinycss2.parse_declaration_list(element.attrib['style'])
+      found_width_100_percent = False
+      found_display_inline_block = False
+      for d in declarations:
+        if check_attribute(d, 'name', 'display') and hasattr(d, 'value') and len(d.value) > 0 and check_attribute(d.value[0], 'lower_value', 'inline-block'):
+          found_display_inline_block = True
+        if check_attribute(d, 'name', 'width') and hasattr(d, 'value') and len(d.value) > 0 and check_attribute(d.value[0], 'type', 'percentage') and check_attribute(d.value[0], 'value', 100):
+          found_width_100_percent = True
+        
+      if found_width_100_percent and found_display_inline_block:
+        new_styles = []
+        for d in declarations:
+          if not (hasattr(d, 'name') and d.name == 'display' and len(d.value) > 0 and d.value[0].lower_value == 'inline-block'):
+            new_styles.append(d.serialize())
+        element.attrib['style'] = ';'.join(new_styles)
+        
+    # insert header to first child body element if possible
+    # we don't insert to body element because in some emails the first element is a page. 
+    # so anything we put before the first element will push the first element to the 2nd page. 
+    header_inserted = False
+    body_element = elements.find('body') or elements.find('*//body')
+    if (body_element != None):
+      header_elements = html5lib.parse(header, namespaceHTMLElements=False)
+      for child in body_element:
+        child.insert(0, header_elements)
+        # if there is text in child, we want the header element to stay before the text
+        header_elements.tail, child.text = child.text, None
+        header_inserted = True
+        break
+
+    # produce the new HTML string after removing IDs attributes
+    s = html5lib.serializer.HTMLSerializer()
+    walker = html5lib.getTreeWalker("etree")
+    stream = walker(elements)
+    output = s.serialize(stream)
+    if not header_inserted:
+      content_string = header
+    else:
+      content_string = ''
+    for item in output:
+      content_string = content_string + item
 
   # log the html for debug purpose
   with open("html.log", "w") as file:
